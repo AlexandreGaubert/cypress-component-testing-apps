fb841cd2d01885615b208bea893f031fd3e4b50c
Alexandre Gaubert <alexandre.gaubert@mergify.com>
2022-10-25 14:17:36 +0200

    Same cypress config than ours

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,26 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: {
+    video: false,
+    viewportHeight: 1080,
+    viewportWidth: 1080,
+    reporter: 'list',
+    numTestsKeptInMemory: 5,
     devServer: {
       framework: 'create-react-app',
-      bundler: 'webpack'
+      bundler: 'webpack',
+    },
+  },
+
+  e2e: {
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    requestTimeout: 30000,
+    defaultCommandTimeout: 10000,
+    retries: {
+      runMode: 2,
+      openMode: 0
     }
-  }
-})
+  },
+});
